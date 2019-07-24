963f134fa66d4f3e69a35a4da0a70704ef0ebf28
hyunkerr <shg7788@likelion.org>
2019-07-24 20:05:20 +0900

    login_html

--- qrating/settings.py
+++ qrating/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'accounts'
+    'accounts',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
