ec2bad955424bba27b8778de2a10e39045c67c42
major96 <jinkeun96@gmail.com>
2019-08-09 22:52:34 +0900

    nickname form edited

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,22 +8,29 @@ class UserRegsiterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1','password2']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
+            'username': forms.TextInput(attrs={'max_length':'15', 'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'password1' : forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2' : forms.PasswordInput(attrs={'class': 'form-control'}),
         }
         labels = {
-            'username': '닉네임',
+            'username': '아이디',
             'email': '이메일',
             'password1': '패스워드',
             'password2': '패스워드 확인',
+            
         }
     
 class ProfileRegsiterForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['nickname']
+        widgets ={
+            'nickname': forms.TextInput(attrs={'class':'form-control', 'placeholder':'12자 이내로 입력 가능합니다'})
+        }
+        labels ={
+            'nickname': '닉네임',
+        }
 
 class LoginForm(AuthenticationForm):
     class Meta:
