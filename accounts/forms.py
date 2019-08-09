a679d0c8e17d260a4bb847fc2fa38d7016453ab8
major96 <jinkeun96@gmail.com>
2019-08-09 21:00:30 +0900

    register and login design first

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,19 @@ class UserRegsiterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1','password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1' : forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2' : forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'username': '닉네임',
+            'email': '이메일',
+            'password1': '패스워드',
+            'password2': '패스워드 확인',
+        }
+    
 
 class ProfileRegsiterForm(forms.ModelForm):
     class Meta:
